25b3771db168482a0fa82e944bb439c597ec28b1
ppradhan78 <pradhan.prasanta78@gmail.com>
2024-06-30 03:15:41 +0530

    Olympic Games Data Analysis

--- src/app/components/data-analysis/data-analysis.component.ts
+++ src/app/components/data-analysis/data-analysis.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-data-analysis',
@@ -10,5 +11,8 @@ import { MatCardModule } from '@angular/material/card';
   styleUrl: './data-analysis.component.css'
 })
 export class DataAnalysisComponent {
-
+   constructor(@Inject(DOCUMENT) private document: Document) { }
+  gotoOlympicDataAnalytics(): void {
+    this.document.location.href = 'https://python-dataanalysis.onrender.com/';
+  }
 }
